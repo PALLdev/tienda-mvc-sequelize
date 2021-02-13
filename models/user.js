1da32c4708ad3798382c9b00e9a3c18cefec1c17
palldev <pblolara21@gmail.com>
2021-02-13 05:08:57 -0300

    agregue user model, new user manualmente al iniciar la app y lo hago obtenible con request en un middleware

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const { DataTypes, Model } = require("sequelize");
+const sequelize = require("../util/database");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      allowNull: false,
+      unique: true,
+      primaryKey: true,
+    },
+    nombre: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: "usuarios", // We need to choose the model name
+  }
+);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorsController = require("./controllers/errors");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 const port = 3000;
@@ -24,17 +26,47 @@ app.use(parser.urlencoded({ extended: false }));
 // dando acceso a mi carpeta public (read only)
 app.use(express.static(path.join(__dirname, "public")));
 
+// PASANDO USER AL REQUEST (user creado manualmente)
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorsController.error404Page);
 
+User.hasMany(Product);
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" }); //user created products
+
 sequelize
+  // .sync({ force: true })
   .sync()
   .then(() => {
-    app.listen(port, () => {
-      console.log(`Server running on localhost:${port}`);
-    });
+    // creando user manualmente
+    return User.findByPk(1)
+      .then((user) => {
+        if (!user) {
+          return User.create({ nombre: "admin", email: "admin@test.com" });
+        }
+        return user;
+      })
+      .then((result) => {
+        console.log(result);
+        app.listen(port, () => {
+          console.log(`Server running on localhost:${port}`);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   })
   .catch((err) => {
     console.log(err);
